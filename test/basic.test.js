a21db389422accef2c7df43893196f352ab63db7
Stan Goldin <48094744+Lomet@users.noreply.github.com>
2025-06-25 15:45:55 +0300

    Revise JS anomaly test

--- test/basic.test.js
+++ test/basic.test.js
@@ -6,3 +6,13 @@ describe("basic math", () => {
     assert.strictEqual(1 + 1, 2);
   });
 });
+
+describe("js anomalies", () => {
+  it("shows floating point weirdness", () => {
+    assert.notStrictEqual(0.1 + 0.2, 0.3);
+  });
+
+  it("reminds us that null is actually an object", () => {
+    assert.strictEqual(typeof null, "object");
+  });
+});
