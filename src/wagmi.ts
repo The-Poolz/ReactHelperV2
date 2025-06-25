79fe866cb189a35f0e9689c1fe4a233d03e7794f
Stan Goldin <48094744+Lomet@users.noreply.github.com>
2025-06-25 18:06:01 +0300

    feat: update wagmi config with poolz chains

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,13 +1,13 @@
+import { createClient } from "viem";
 import { http, createConfig } from "wagmi";
-import { bsc } from "wagmi/chains";
+import { avalanche, avalancheFuji, bsc, bscTestnet, lukso, mainnet, polygon, polygonMumbai } from "wagmi/chains";
 import { coinbaseWallet, injected, walletConnect } from "wagmi/connectors";
-import { createClient } from 'viem'
 
 export const config = createConfig({
-  chains: [bsc], //poolz main chain
+  chains: [mainnet, bsc, polygon, bscTestnet, lukso, polygonMumbai, avalanche, avalancheFuji], //poolz chains
   connectors: [injected(), coinbaseWallet(), walletConnect({ projectId: import.meta.env.VITE_WC_PROJECT_ID })],
   client({ chain }) {
-    return createClient({ chain, transport: http() })
+    return createClient({ chain, transport: http() });
   },
 });
 
