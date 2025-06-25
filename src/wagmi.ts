73f975a840c1f0e07638d32e6e832f42c6278f5d
Stan Goldin <48094744+Lomet@users.noreply.github.com>
2025-06-25 13:15:08 +0300

    Remove wagmi usage of Poolz data

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,25 +1,18 @@
 import { http, createConfig } from 'wagmi'
 import { mainnet, sepolia } from 'wagmi/chains'
 import { coinbaseWallet, injected, walletConnect } from 'wagmi/connectors'
-import { poolzChains } from './poolzData.ts'
-
-const knownChains = {
-  [mainnet.id]: mainnet,
-  [sepolia.id]: sepolia,
-}
-
-const chains = poolzChains.map((id) => knownChains[id]).filter(Boolean)
-
-if (chains.length === 0) chains.push(mainnet, sepolia)
 
 export const config = createConfig({
-  chains,
+  chains: [mainnet, sepolia],
   connectors: [
     injected(),
     coinbaseWallet(),
     walletConnect({ projectId: import.meta.env.VITE_WC_PROJECT_ID }),
   ],
-  transports: Object.fromEntries(chains.map((c) => [c.id, http()])),
+  transports: {
+    [mainnet.id]: http(),
+    [sepolia.id]: http(),
+  },
 })
 
 declare module 'wagmi' {
