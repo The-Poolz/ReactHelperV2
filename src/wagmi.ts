ab50e487b29b96878a1867194ce11acfc7b9277b
Tai Nguyen <119270070+0xnatcch@users.noreply.github.com>
2025-07-10 17:48:49 +0700

    ci: uncomment wallet connect (#42)
    Co-authored-by: natcch <thanhtaipkl@gmail.com>
    Co-authored-by: Stan Goldin <48094744+Lomet@users.noreply.github.com>

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -3,11 +3,11 @@ import { http, createConfig } from "wagmi";
 import { unichain, telos, polygon, moonbeam, avalanche, mantaTestnet, manta, mainnet, bscTestnet, sepolia, base, bsc, arbitrum } from "wagmi/chains";
 import { coinbaseWallet, injected, walletConnect } from "wagmi/connectors";
 
-// const walletConnectProjectId = import.meta.env.VITE_WC_PROJECT_ID;
+const walletConnectProjectId = import.meta.env.VITE_WC_PROJECT_ID;
 
-// if (!walletConnectProjectId) {
-//   throw new Error("VITE_WC_PROJECT_ID environment variable is required");
-// }
+if (!walletConnectProjectId) {
+  throw new Error("VITE_WC_PROJECT_ID environment variable is required");
+}
 
 export const config = createConfig({
   chains: [unichain, telos, polygon, moonbeam, avalanche, mantaTestnet, manta, mainnet, bscTestnet, sepolia, base, bsc, arbitrum], //poolz chains
