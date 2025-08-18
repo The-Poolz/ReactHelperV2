0b03e2c6a9ae9cbd706b024d4fb271dbef6ed0df
Tai Nguyen <thanhtaipkl@gmail.com>
2025-08-18 22:54:38 +0700

    chore: export wallet client from wagmi

--- src/hooks/usePoolzApp.ts
+++ src/hooks/usePoolzApp.ts
@@ -1,11 +1,17 @@
-import { useAccount,useChainId,useChains,useSwitchChain, useWatchAsset } from "wagmi";
+import { useMemo } from "react";
+import { useAccount,useChainId, useSwitchChain, useWatchAsset } from "wagmi";
+import { config } from "../wagmi";
+import { Chain } from "viem/chains";
 
 export function usePoolzApp() {
   const chainId = useChainId();
-  const chain = useChains().find(c => c.id === chainId);
   const account = useAccount();
   const { switchChainAsync, isPending } = useSwitchChain();
   const watchAsset = useWatchAsset()
+
+  const chain = useMemo(() => {
+    return config.chains.find((c: Chain) => c.id === chainId);
+  }, [chainId, config.chains]);
 
   return {
     ...account,
