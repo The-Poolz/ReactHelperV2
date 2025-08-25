fdd3fb224cc47a843b643313d89d14681b83498c
Tai Nguyen <119270070+0xnatcch@users.noreply.github.com>
2025-08-25 22:36:29 +0700

    chore: export types and handle disconnect wallet (#69)
    Co-authored-by: Tai Nguyen <thanhtaipkl@gmail.com>

--- src/hooks/usePoolzApp.ts
+++ src/hooks/usePoolzApp.ts
@@ -1,17 +1,42 @@
 import { useMemo } from "react";
-import { useAccount,useChainId, useSwitchChain, useWatchAsset } from "wagmi";
+import { useAccount, useChainId, useDisconnect, useSwitchChain, useWatchAsset } from "wagmi";
 import { config } from "../wagmi";
 import { Chain } from "viem/chains";
+import { watchAccount } from "wagmi/actions";
+
+export function watchAccountChange(callback: (account: any) => void) {
+  return watchAccount(config, {
+    onChange: callback,
+  });
+}
 
 export function usePoolzApp() {
   const chainId = useChainId();
   const account = useAccount();
+  const { disconnect } = useDisconnect()
   const { switchChainAsync, isPending } = useSwitchChain();
-  const watchAsset = useWatchAsset()
+  const watchAsset = useWatchAsset();
 
   const chain = useMemo(() => {
     return config.chains.find((c: Chain) => c.id === chainId);
   }, [chainId, config.chains]);
+
+  const disconnectWallet = async () => {
+    disconnect()
+    if(typeof window === 'undefined') return
+    try {
+      await window.ethereum.request({
+        method: "wallet_revokePermissions",
+        params: [
+          {
+            eth_accounts: {}
+          }
+        ]
+      })
+    } catch (error) {
+      console.error("Error disconnecting wallet:", error)
+    }
+  }
 
   return {
     ...account,
@@ -20,6 +45,7 @@ export function usePoolzApp() {
     account: account.address,
     switchChainAsync,
     isSwitching: isPending,
-    watchAsset
+    watchAsset,
+    disconnectWallet
   };
 }
